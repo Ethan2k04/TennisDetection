58daf55829fba59528d1eb8565cb1aa06ba35bc0
Ethan2k04 <3287816638@qq.com>
2025-01-30 14:10:36 +0800

    update README.md

--- kernel.py
+++ kernel.py
@@ -198,6 +198,7 @@ def has_minimum(sizes):
     if len(sizes) < 3:
         return False  # 数组长度小于3，无法形成极小值
 
+    # 判断某点是否为局部最小值，并且是全局前三小的
     for i in range(1, len(sizes) - 1):
         if sizes[i] < sizes[i - 1] and sizes[i] < sizes[i + 1] and sizes[i] <= sorted(sizes)[2]:
             return True  # 找到极小值
